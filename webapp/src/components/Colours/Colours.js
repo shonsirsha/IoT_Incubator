6f78bf70f2cabbf64b4a58fe3b565c8df6e0fc64
shonsirsha <seangeekpro@gmail.com>
2021-04-11 02:04:13 +0200

    display status for detail incubator view

--- webapp/src/components/Colours/Colours.js
+++ webapp/src/components/Colours/Colours.js
@@ -4,3 +4,4 @@ export const darkerSecondary = "#B1B1B1";
 export const lighterSecondary = "#F3F3F3";
 export const darkGrey = "#ACACAC";
 export const danger = "#C20B0B";
+export const darkPrimary = "#009F5C";
--- webapp/src/components/Views/DetailIncubator.js
+++ webapp/src/components/Views/DetailIncubator.js
@@ -1,6 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import IncubatorContext from "../../context/incubator/incubatorContext";
-
+import styled from "styled-components";
+import { BodyNormal, HeadingXS } from "../Typography/Typographies";
+import { danger, darkPrimary } from "../Colours/Colours";
+const StyledBodyNormal = styled(BodyNormal)`
+	color: ${(props) => (props.active ? darkPrimary : danger)};
+`;
+const StyledHeadingXS = styled(HeadingXS)`
+	color: ${(props) => (props.active ? darkPrimary : danger)};
+`;
 const DetailIncubator = ({ deviceDetailData }) => {
 	const incubatorContext = useContext(IncubatorContext);
 	const [deviceDetail, setDeviceDetail] = useState(null);
@@ -13,7 +21,22 @@ const DetailIncubator = ({ deviceDetailData }) => {
 		setDeviceDetail(arr[0]);
 	}, [incubators]);
 
-	return <div>{deviceDetail && deviceDetail.deviceName}</div>;
+	return (
+		<>
+			{deviceDetail && (
+				<div className="d-flex flex-column">
+					<div className="d-flex align-items-center">
+						<StyledBodyNormal active={deviceDetail.active}>
+							Status:{" "}
+						</StyledBodyNormal>
+						<StyledHeadingXS className="mt-1 ml-2" active={deviceDetail.active}>
+							{deviceDetail.active ? `ONLINE / ACTIVE` : `OFFLINE / INACTIVE`}
+						</StyledHeadingXS>
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default DetailIncubator;
